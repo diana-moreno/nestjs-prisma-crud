ac42bd776080365ff84bb242c37215b617792b13
Diana <dianamorenoarranz@gmail.com>
2022-07-04 16:12:22 +0200

    Seed database with products

---
+++ prisma/seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+import { products } from '../products';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (let product of products) {
+    await prisma.product.create({
+      data: product,
+    });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
